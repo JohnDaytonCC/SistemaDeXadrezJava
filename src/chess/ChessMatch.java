5499e5c741d8dbe38087ebfdfc3ca1f2a4a51e06
JohnDaytonCC <john.dayton.cc@gmail.com>
2022-01-24 14:45:49 -0300

    Correção do tratamento do movimento especial - en passant

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -174,17 +174,19 @@ public class ChessMatch {
 		}
 		
 		// Tratamento do movimento especial: en passant
-		if(source.getColumn() != target.getColumn() && capturedPiece == null) {
-			Position pawnPosition;
-			if(p.getColor() == Color.WHITE) {
-				pawnPosition = new Position(target.getRow() + 1, target.getColumn());			
+		if(p instanceof Pawn) {
+			if(source.getColumn() != target.getColumn() && capturedPiece == null) {
+				Position pawnPosition;
+				if(p.getColor() == Color.WHITE) {
+					pawnPosition = new Position(target.getRow() + 1, target.getColumn());			
+				}
+				else {
+					pawnPosition = new Position(target.getRow() - 1, target.getColumn());
+				}
+				capturedPiece = board.removePiece(pawnPosition);
+				capturedPieces.add(capturedPiece);
+				piecesOnTheBoard.remove(capturedPiece);
 			}
-			else {
-				pawnPosition = new Position(target.getRow() - 1, target.getColumn());
-			}
-			capturedPiece = board.removePiece(pawnPosition);
-			capturedPieces.add(capturedPiece);
-			piecesOnTheBoard.remove(capturedPiece);
 		}
 		
 		return capturedPiece;
@@ -220,16 +222,18 @@ public class ChessMatch {
 		}
 		
 		// Tratamento do movimento especial: en passant
-		if(source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable) {
-			ChessPiece pawn = (ChessPiece)board.removePiece(target);
-			Position pawnPosition;
-			if(p.getColor() == Color.WHITE) {
-				pawnPosition = new Position(3, target.getColumn());			
+		if(p instanceof Pawn) {
+			if(source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable) {
+				ChessPiece pawn = (ChessPiece)board.removePiece(target);
+				Position pawnPosition;
+				if(p.getColor() == Color.WHITE) {
+					pawnPosition = new Position(3, target.getColumn());			
+				}
+				else {
+					pawnPosition = new Position(4, target.getColumn());
+				}
+				board.placePiece(pawn, pawnPosition);
 			}
-			else {
-				pawnPosition = new Position(4, target.getColumn());
-			}
-			board.placePiece(pawn, pawnPosition);
 		}
 	}
 	
